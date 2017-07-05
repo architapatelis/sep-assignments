bed649e247f268b6a8b539635deccfdba2a78501
Archita Patel <architapatelis@gmail.com>
2017-07-05 13:37:14 -0700

    solve line and screen problems using array data structure

--- 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
+++ 01-data-structures/01-introduction-to-data-structures/screen/screen.rb
@@ -6,18 +6,34 @@ class Screen
   attr_accessor :matrix
 
   def initialize(width, height)
+    @matrix = Array.new(width) { Array.new(height) }
+    @width = width
+    @height = height
   end
 
   # Insert a Pixel at x, y
   def insert(pixel, x, y)
+    if inbounds(x, y)
+      matrix[x][y] = pixel
+    end
   end
 
   def at(x, y)
+    if inbounds(x,y)
+      return matrix[x][y]
+    else
+      return nil
+    end
   end
 
   private
 
   def inbounds(x, y)
+    is_valid = true;
+    if (x < 0 || x > width - 1) || (y < 0 || y > height - 1)
+      is_valid = false
+    end
+    return is_valid
   end
 
 end
